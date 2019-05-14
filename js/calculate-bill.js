30aa2574bfce085eb30205d32780a1315c1c549a
Thando <nodwengut@gmail.com>
2019-05-14 13:52:17 +0200

    initial commit

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,13 +1,54 @@
 //get a reference to the calculate button
+var calculateBtnElement = document.querySelector('.calculateBtn');
+
 
 //get a reference to the billTotal element
+var billTotalElement = document.querySelector('.billTotal');
+var totalElement = document.querySelector('.total');
 
 //get a reference to the billString
+var billStringElement = document.querySelector(".billString");
 
 //create the function that will be called when the calculate button is pressed
 //  * this function should read the string value entered - split it on a comma.
-//  * loop over all the entries in the the resulting list
+//  * loop over all the entries in the resulting list
 //  * check if it is a call or an sms and add the right amount to the overall total
 //  * once done looping over all the entries - display the total onto the screen in the billTotal element
 
+function calculateBtnClicked(callsAndSms){
+   var callsAndSmsArr = callsAndSms.split(",");
+    
+   var callsCounter = 0;
+   var smsCounter = 0;
+   for(var i = 0; i < callsAndSmsArr.length; i++) {
+      var element = callsAndSmsArr[i].trim();
+      element = element.toLowerCase();
+
+      if(element == "sms") {
+         smsCounter++;	
+      } else if(element == "call") {
+         callsCounter++;
+      } else if(element == "") {
+         return (0.00).toFixed(2);
+      }else {
+         alert("Invalid input data");
+      }
+   }
+   return (callsCounter * 2.75 + smsCounter * 0.75).toFixed(2);
+
+}
+
 //link the function to a click event on the calculate button
+calculateBtnElement.addEventListener('click', function() {
+   var string = billStringElement.value;
+   var totalBill = calculateBtnClicked(string);
+   billTotalElement.innerHTML = totalBill;
+
+   if(totalBill > 30) {
+      totalElement.classList.remove('warning');
+      totalElement.classList.add('danger');
+   } else if(totalBill > 20 && totalBill < 30){
+      totalElement.classList.remove('danger');
+      totalElement.classList.add('warning');
+   }
+});
